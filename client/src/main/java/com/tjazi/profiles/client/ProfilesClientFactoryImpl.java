801168237160fed49b90e8e8c5e6f1191c2459ef
Krzysztof Wasiak <krzysztof.wasiak@wipro.com>
2015-11-03 09:20:05 +0000

    Chaning Service to Component descriptor on ProfilesClientFactoryImpl

--- client/src/main/java/com/tjazi/profiles/client/ProfilesClientFactoryImpl.java
+++ client/src/main/java/com/tjazi/profiles/client/ProfilesClientFactoryImpl.java
@@ -1,6 +1,7 @@
 package com.tjazi.profiles.client;
 
 import com.tjazi.lib.messaging.rest.RestClientImpl;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.net.URI;
@@ -9,7 +10,7 @@ import java.net.URI;
  * Created by Krzysztof Wasiak on 10/10/15.
  */
 
-@Service
+@Component
 public class ProfilesClientFactoryImpl implements ProfilesClientFactory {
 
     public ProfilesClient createProfilesClient(URI targetUri) {
